c8943857ff349c5b1d36ebeb5eed2403722eb25f
Hamish Johnson <hamish@magpiedigital.org>
2015-11-04 13:20:54 -0800

    Changes navigation to depend only on css and not on widthHeight directive. Uses border trapezoids for better presentation.
    widthHeight directive moved to legacy. Updated README.md

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 an existential browser game
 
 To do:
-- [ ] Replace widthHeight functionality (Adjusts the rotated left and right nav bars so that we don't need to use jQuery)
+- [x] Replace widthHeight functionality (Adjusts the rotated left and right nav bars so that we don't need to use jQuery)
--- assets/js/directives.js
+++ assets/js/directives.js
@@ -1,27 +1,46 @@
-pointless.directive('widthHeight', function() {
-    function link(scope, element, attrs) {
-    	var h = window.innerHeight;
-    	$(element).width(h);
-    
-        var addEvent = function(object, type, callback) {
-        	if (object == null || typeof(object) == 'undefined') return;
-        	if (object.addEventListener) {
-        		object.addEventListener(type, callback, false);
-        	} else if (object.attachEvent) {
-        		object.attachEvent("on" + type, callback);
-        	} else {
-        		object["on"+type] = callback;
-        	}
-        };
 
-        addEvent(window, "resize", function(event) {
-        	h = window.innerHeight;
-        	$(element).width(h);
-        	console.log(h);
-        });
-    };
-    return {
-        restrict: 'EA',
-        link: link
-    };
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// == LEGACY == //
+
+// // Changes width of rotated nav bars with height on load and on resize
+// pointless.directive('widthHeight', function() {
+//     function link(scope, element, attrs) {
+//     	var h = window.innerHeight;
+//     	$(element).width(h);
+    
+//         // Event listener for resizing
+//         var addEvent = function(object, type, callback) {
+//         	if (object == null || typeof(object) == 'undefined') return;
+//         	if (object.addEventListener) {
+//         		object.addEventListener(type, callback, false);
+//         	} else if (object.attachEvent) {
+//         		object.attachEvent("on" + type, callback);
+//         	} else {
+//         		object["on"+type] = callback;
+//         	}
+//         };
+
+
+//         addEvent(window, "resize", function(event) {
+//         	h = window.innerHeight;
+//         	$(element).width(h);
+//         	console.log(h);
+//         });
+//     };
+//     return {
+//         restrict: 'EA',
+//         link: link
+//     };
+// });
